6c9520013dd13649d5302747e8dc06e0ed4e1a91
hiroshimuto <hiroshi.github@gmail.com>
2018-11-29 18:30:40 +0900

    Set application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,15 @@ module JobApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    config.i18n.default_locale = :ja
+    end
+  end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
