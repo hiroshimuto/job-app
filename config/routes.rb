54c7001ff880ece9be07d46961f4bb3e1fed2699
hiroshimuto <hiroshi.github@gmail.com>
2018-11-29 19:58:10 +0900

    Set up actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'jobs/index'
-  get 'jobs/new'
-  get 'jobs/create'
-  get 'jobs/show'
   root 'homes#index'
+  resources :jobs, only: [:index, :new, :create, :show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
