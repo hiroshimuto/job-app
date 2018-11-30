87d0b366c748edd8cd8ba69131fe74d0d63355b1
hiroshimuto <hiroshi.github@gmail.com>
2018-11-30 17:08:12 +0900

    Create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'entries/create'
   root 'homes#index'
-  resources :jobs, only: [:index, :new, :create, :show]
+  resources :jobs, only: [:index, :new, :create, :show] do
+    resources :entries
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
